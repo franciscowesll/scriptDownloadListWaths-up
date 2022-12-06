14b61691db13d5555fa9852053aa6d4709253d58
Francisco Weslley <84885942+franciscowesll@users.noreply.github.com>
2022-12-06 16:38:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function Whats(){
     var str = "Quero um script para baixar os contatos do grupo do whats'up"
     if (str == true){
-        console.log('Entre em contato comigo: 99 981753040');
+        console.log('Entre em contato comigo: +55 99 981753040') // esse número é também do whats
     }
     else {
     console.log('Tudo bem, fique em paz')
