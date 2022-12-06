d8d9d0e847cbbe66382909ae81051c2068168396
Francisco Weslley <84885942+franciscowesll@users.noreply.github.com>
2022-12-06 16:37:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-const { encode } = require("punycode");
-function rb() {
-    var a = document.createElement('button')
-        a.innerHTML = 'DOWNLOAD',
-        a.style.background = '#44c767',
-        a.style.backgroundImage = 'linear-CanvasGradient(#44c767, #64e787)',
-        a.style.borderRadius = '50px',
-        a.style.display = 'inline-block',
-        a.style.color = '#fff',
-        a.style.fontSize = '17px',
-        a.style.padding = '11px 31px',
-        a.style.position = 'fixed',
-        a.style.right = '10px',
-        a.style.top = '7px',
-        a.style.zIndex = '999'
 
-    var b = document.getElementsByTagName('body')[0]
-        b.appendChild(a)
-        let paternOne = "/[\|))({(|/g"
-        let paternTwo = "//g"
-        a.addEventListener('click', function(){
-           es = document.querySelector(".zzgSd._3e6xi span").title.replace(/,/g,"\r\n").replace('Você', '').replace(paternOne, '')
-           es = es.replace(paternTwo, '').replace(/-/g,'')
-           ue="data:Application/octet-stream,"+encodeURIComponent(es)
-           newWindow = window.open(ue,"file") 
-        })
+function Whats(){
+    var str = "Quero um script para baixar os contatos do grupo do whats'up"
+    if (str == true){
+        console.log('Entre em contato comigo: 99 981753040');
+    }
+    else {
+    console.log('Tudo bem, fique em paz')
+    }
 }
-rb()
+Whats()
